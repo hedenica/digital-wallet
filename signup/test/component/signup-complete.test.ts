72b28eaa22197e9efec169595790f427b5a19e91
hedenica <hedenica@hotmail.com>
2020-06-04 07:44:00 -0300

    ✅  updating tests on signup

--- signup/test/component/signup-complete.test.ts
+++ signup/test/component/signup-complete.test.ts
@@ -3,14 +3,17 @@ import signupComplete from '@src/controllers/signup-complete';
 import { SinonStub, stub, restore } from "sinon";
 import signupRepo from '@src/ports/repos/signup';
 import Signup from '@src/types/signup';
+import signupNotifier from '@src/ports/notifiers/signup'
 
 let getByTokenSignup: SinonStub;
 let updateStatusSignup: SinonStub;
+let completeNotificationSignup: SinonStub;
 
 describe("Signup complete", () => {
   beforeEach(() => {
     getByTokenSignup = stub(signupRepo, "getByToken");
     updateStatusSignup = stub(signupRepo, "updateStatus");
+    completeNotificationSignup = stub(signupNotifier, "complete");
   });
 
   afterEach(() => restore());
@@ -22,6 +25,15 @@ describe("Signup complete", () => {
     await signupComplete(token);
 
     expect(updateStatusSignup).to.have.been.calledWith(signup, "COMPLETE");
+  });
+
+  it("send a notification when a signup is completed", async () => {
+    const token = "some-token";
+    getByTokenSignup.resolves(signup);
+
+    await signupComplete(token);
+
+    expect(completeNotificationSignup).to.have.been.calledOnce;
   });
 });
 
